ee6f890395e436b7ca8f75badab0076bc3d56138
Rahul Pathak <rahulzha93@gmail.com>
2019-11-22 21:09:33 +0545

    web Initials

---
+++ JavaScript_fop2/external.js
@@ -0,0 +1,3 @@
+function demo(){
+    document.querySelector('h1').innerHTML = "I'm Clicked!";
+}
---
+++ JavaScript_fop2/myjs.js
@@ -0,0 +1,4 @@
+function demo1(){
+    document.querySelector('h1').innerHTML = "Hello";
+    document.querySelector('h1').style.color = "black";
+}
